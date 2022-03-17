008d353f222460c84499d22a10913a263d1512e6
Dawid Sula <dawidsula26@gmail.com>
2022-03-17 10:10:13 +0100

    Removed unnecessary asserts

--- crabsquared-checkers/api/api.py
+++ crabsquared-checkers/api/api.py
@@ -11,7 +11,6 @@ def api_base():
 
 @app.route('/available_moves', methods=['POST'])
 def get_avaliable_moves():
-    assert (request.json['type'] == 'board')
     board = request.json['state']
     return {
         'type': 'available_moves', 
@@ -22,7 +21,6 @@ def get_avaliable_moves():
 
 @app.route('/bot_move', methods=['POST'])
 def get_bot_move():
-    assert (request.json['type'] == 'board')
     board = request.json['state']
     return {
         'type': 'bot_move',
