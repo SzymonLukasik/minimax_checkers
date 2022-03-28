76445bed47c9ca34a293a1add37fef506e593050
Pawel Balawender <pbalawender101@gmail.com>
2022-03-28 15:15:14 +0200

    dodaj printery tutaj

--- crabsquared-checkers/engine/tests/utils.h
+++ crabsquared-checkers/engine/tests/utils.h
@@ -1,18 +1,55 @@
 #ifndef CRABSQUARED_CHECKERS_UTILS_H
 #define CRABSQUARED_CHECKERS_UTILS_H
 
-#define assertEqual( ... )               \
-do {                                            \
-    if( !( __VA_ARGS__ ) ) {                     \
-        std::cerr << "Unit test assert [ " \
-        << ( #__VA_ARGS__ )             \
-        << " ] failed in line [ "       \
-        << __LINE__                     \
-        << " ] file [ "                 \
-        << __FILE__ << " ]"             \
-        << std::endl;                     \
-        err_code = 1;                           \
-    }                                            \
-} while( false )
+/*
+    Converts pos to square name.
+    Eg: (1,2) -> "C2"
+        (3,5) -> "F4"
+*/
+inline std::string sqtostr(square_t sq)
+{
+    return {char(sq.second + char('A')), char(sq.first + char('1'))};
+}
+
+std::ostream &operator<<(std::ostream &os, square_t sq)
+{
+    os << sqtostr(sq);
+    return os;
+}
+
+template <typename T>
+std::ostream& operator<<(std::ostream& os, const std::list<T>& x) {
+    os << "[|";
+    if (!x.empty()) {
+        for (auto it=x.begin(); it != std::prev(x.end()); ++it) {
+            os << *it << ", ";
+        }
+        auto it = std::prev(x.end());
+        os << *it;
+    }
+    os << "|]";
+    return os;
+}
+
+template <typename U, typename V>
+std::ostream& operator<< (std::ostream& os, const std::pair<U, V>& x) {
+    os << '<' << x.first << ", " << x.second << '>';
+    return os;
+}
+
+template <typename U, typename V>
+std::ostream& operator<<(std::ostream& os, const std::map<U, V>& x)
+{
+    os << '{';
+    if (x.size() > 0) {
+        for (auto it=x.begin(); it != std::prev(x.end()); ++it) {
+            os << it->first << ": " << it->second << ", ";
+        }
+        auto it = std::prev(x.end());
+        os << it->first << ": " << it->second;
+    }
+    os << '}';
+    return os;
+}
 
 #endif //CRABSQUARED_CHECKERS_UTILS_H
