9ab5ab6b916d45dfe00b2e320d180f7740dadd98
Pawel Balawender <pbalawender101@gmail.com>
2022-03-31 13:45:22 +0200

    dodaj dziwne testy

---
+++ crabsquared-checkers/engine/tests/test_not_rectangular.cpp
@@ -0,0 +1,19 @@
+#include <string>
+#include "../engine.h"
+#include "utils.h"
+#include "test_driver.h"
+
+using namespace std;
+
+const board_t board0 = {
+  {NP, NP, BK, NP, NP},
+  {NP, WK, WP, NP, NP},
+  {NP, NP, NP, BP, NP},
+};
+const piece_color_t col0 = B;
+const std::string result0 = "{C1: [|[|C1, A3|]|], D3: [|[|D3, B1|]|]}";
+
+
+const std::vector<test_data> tests = {
+  TEST(0)
+};
