c8832aeca80b862788c4cd7e9f5aa87d243fc6e3
Jakub Podolak <jakub.podolak.241@gmail.com>
2022-05-02 16:53:31 +0200

    Fix c++17 version

--- crabsquared-checkers/engine/python/setup.py
+++ crabsquared-checkers/engine/python/setup.py
@@ -1,5 +1,13 @@
 from setuptools import setup
 
 from Cython.Build import cythonize
+from Cython.Distutils.extension import Extension
 
-setup(ext_modules=cythonize("engine.pyx"))
+setup(
+    ext_modules=cythonize(Extension(
+        "engine",
+        ["engine.pyx"],
+        language="c++",
+        extra_compile_args=["-std=c++17"]
+    ))
+)
