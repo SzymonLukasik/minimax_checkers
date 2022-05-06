7e3258ebe440a7c9d5563c835f51dd708b3dd57a
Dawid Sula <dawidsula26@gmail.com>
2022-05-06 19:05:26 +0200

    Added comment

--- crabsquared-checkers/bot/bot_abstract.py
+++ crabsquared-checkers/bot/bot_abstract.py
@@ -1,5 +1,8 @@
 from abc import ABC, abstractmethod
 
+# Class repsenting checkers bot.
+# All subclasses must be added to bot_manager or they will be unusable.
+# Only one instance of a subclass can be created.
 class bot_abstract(ABC):
     @abstractmethod
     def get_name(self):
